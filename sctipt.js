060d6697e1884ab5be44f966b8506bbf013f90ae
dilshodtm <dilshodtm97@gmail.com>
2022-10-11 15:14:36 +0500

    java

---
+++ sctipt.js
@@ -0,0 +1,57 @@
+
+// 1
+// let L = Number(prompt("SM ni kiriting"));
+// let sm = 100;
+// let result = L / sm;
+// console.log(${result} metr);  
+
+
+// 2
+// let M = Number(prompt("Kg ni kiriting"));
+// let kg = 1000;
+// let result = M / kg;
+// console.log(${result} tonna);
+
+// 3
+// let a = Number(prompt("1-sonni chiqaring"));
+// let b = Number(prompt("2-sonni chiqaring"));
+
+// let result = a / b;
+// console.log(${a} sonini ichida ${b} sonini ${Math.trunc(result)} marta joylashtirish mumkin);
+
+// 4
+// let a = Number(prompt("1-sonni chiqaring"));
+// let b = a > 9 ? Bu o'nlik son ${a} : Bu o'nlik son emas Bu birlik son${a};
+// let c = Math.floor(a / 10);
+// let d = a % 10;
+// console.log(`1-o'nlik son: ${c}; 
+// 2-birlik son: ${d}; 
+// ${b}`);
+
+// 5
+// let a = Number(prompt("1-sonni chiqaring"));
+// let b = a > 9 ? true : false;
+// let c = Math.floor(a / 10);
+// let d = a % 10;
+// console.log(c + d);
+
+// 6
+// let a = Number(prompt("1-sonni chiqaring"));
+// let b = a > 9 ? true : false;
+// let c = Math.floor(a / 10);
+// let d = a % 10;
+// console.log(c * d);
+
+// 7
+// let a = Number(prompt("1-sonni chiqaring"));
+// let b = Math.floor(a % 1000);
+// let c = a > 99 ? Bu yuzlik son ${b} : Bu yuzlik son emas ${b};
+// console.log(c); 
+
+
+// 8
+// let a = Number(prompt("1-sonni chiqaring"));
+// let b = a / 3600;
+// let c = a / 60;
+// console.log(b + " " + "soat o'tdi");
+// console.log(c + " minut o'tdi");
